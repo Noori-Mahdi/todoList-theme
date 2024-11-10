2259e5e8fcbffa4801f82fe8a45673d034f2e64b
mahdi <mahdi@gmail.com>
2024-11-10 19:05:14 +0330

    debug

--- src/page/TodoPage/TodoPage.tsx
+++ src/page/TodoPage/TodoPage.tsx
@@ -8,7 +8,7 @@ export const TodoPage = () => {
     const { theme, toggleTheme } = useTheme();
 
     return ( 
-        <div className={`flex relative justify-center w-screen h-screen ${theme === 'dark' ? 'bg-main-dark': 'bg-main-light'}`} >
+        <div className={`flex relative justify-center w-screen min-h-screen ${theme === 'dark' ? 'bg-main-dark': 'bg-main-light'}`} >
             <div className='absolute w-full h-48 z-0'> 
                 <img className='w-full h-full' src={theme === 'dark' ? bgDesktopDark : bgDesktopLight}/>
             </div>
